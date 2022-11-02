ce90b9cb7aa4cc0ebae309dffd8818f54469a7cf
Andreas Bigger <abigger87@gmail.com>
2022-11-02 11:40:12 -0400

    validate simulation bundles

--- bot/src/relayer.rs
+++ bot/src/relayer.rs
@@ -1,19 +1,27 @@
 //! A relayer module
 
 use ethers::prelude::*;
-use ethers_flashbots::BundleRequest;
+use ethers_flashbots::{BundleRequest, BundleTransaction, SimulatedBundle};
 
 /// Helper function to help catch the various ways errors can be thrown from simulation
 /// This helper function is needed as simulation response has many ways where the
 /// error can be thrown.... which is not documented
-// pub fn check_simulated_response(sim: &SimulatedBundle) -> eyre::Result<()> {
-//     // TODO::::
-//     Ok(())
-// }
+pub fn validate_simulation_response(sim: &SimulatedBundle) -> eyre::Result<()> {
+    // Make sure no simulated bundle transactions have errors or reverts
+    for tx in &sim.transactions {
+        if let Some(e) = &tx.error {
+            eyre::bail!("Error in bundled transaction: {:?}", e);
+        }
+        if let Some(r) = &tx.revert {
+            eyre::bail!("Transaction reverts: {:?}", r);
+        }
+    }
+    Ok(())
+}
 
 /// Construct a Bundle Request for FlashBots
-pub fn construct_bundle(
-    signed_transactions: &Vec<Bytes>,
+pub fn construct_bundle<T: Into<BundleTransaction>>(
+    signed_transactions: Vec<T>,
     block_number: U64,
 ) -> eyre::Result<BundleRequest> {
     // Create the ethers-flashbots bundle request
@@ -21,7 +29,8 @@ pub fn construct_bundle(
 
     // Sign the transactions and add to the bundle
     for tx in signed_transactions {
-        bundle_request = bundle_request.push_transaction(tx.clone());
+        let bundled: BundleTransaction = tx.into();
+        bundle_request = bundle_request.push_transaction(bundled);
     }
 
     // Set other bundle parameters
@@ -33,32 +42,3 @@ pub fn construct_bundle(
     // Return the constructed bundle request
     Ok(bundle_request)
 }
-
-// export const sanityCheckSimulationResponse = (sim) => {
-//   // Contains first revert
-//   if (sim.firstRevert) {
-//     throw new Error(sim.firstRevert.revert);
-//   }
-
-//   // Contains first revert
-//   if (sim.firstRevert) {
-//     throw new Error(sim.firstRevert.revert);
-//   }
-
-//   // Simulation error type
-//   const simE = sim;
-//   if (simE.error) {
-//     throw new Error(simE.error.message);
-//   }
-
-//   // Another type of silent error
-//   // This has to be checked last
-//   const errors = sim.results
-//     .filter((x) => x.error !== undefined)
-//     .map((x) => x.error + " " + (x.revert || ""));
-//   if (errors.length > 0) {
-//     throw new Error(errors.join(", "));
-//   }
-
-//   return sim;
-// };
